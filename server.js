dcb8262ef43c54d2b1cf95359c34465130cca0ee
Paul Campbell <paul.campbell@stonybrook.edu>
2014-09-20 12:44:14 -0400

    Server
    Added express, and socket.io.

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require("express");
-var sockio = require("socket.io");
+var path = require('path');
 var sql = require("sqlite3");
-var sshfs = require("sshfs-node");
 var fs = require("fs");
-var walk = require("walk");
 
+/* Config imports */
+var config = require('./config');
 
+/* Create the web app */
+var app = express(), 
+	server = require('http').createServer(app), 
+	io = require('socket.io').listen(server);
 
+/* Set the directory where static content lives (aka root directory) */
+app.use(express.static(path.join(__dirname, config.root)));
+
+/* Start the server */
+server.listen(config.port);
+
+/* Log the port node is running on */
+console.log('http://localhost:' + config.port);
+
+/* Load the index page */
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+/* Hello, World socket connection */
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
