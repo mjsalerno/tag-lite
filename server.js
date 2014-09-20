d17af91e4c2994d8cbc2ae1bd2998c06c55b0e16
Paul Campbell <paul.campbell@stonybrook.edu>
2014-09-20 12:01:46 -0400

    Server.js
    Fixed js errors.

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require("express");
 var sockio = require("socket.io");
 var sql = require("sqlite3");
 var sshfs = require("sshfs-node");
-var = require("fs");
+var fs = require("fs");
 var walk = require("walk");
-
-"use strict";
 
 
 
