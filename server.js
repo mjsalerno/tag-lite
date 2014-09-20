b398aea9c59b1e45f3d304913e70664c2b1c4107
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-09-20 12:45:45 -0400

    got server to print out txt files for testing

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var sockio = require("socket.io");
 var sql = require("sqlite3");
-var sshfs = require("sshfs-node");
 var fs = require("fs");
-var walk = require("walk");
+var S = require('string');
 
+var walkSync = require('walk-sync');
+var paths = walkSync('.');
 
-
+for ( var i = 0; i < paths.length; i++) {
+  if (S(paths[i]).endsWith(".txt")) {
+    console.log(paths[i])
+  };
+};
