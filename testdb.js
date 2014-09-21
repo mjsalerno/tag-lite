f39935b23fc5989fdf18aa51eab3c251e3abeea1
Scott Harvey <scott.harvey@stonybrook.edu>
2014-09-21 00:47:35 -0400

    Make mike happier.

---
+++ testdb.js
@@ -0,0 +1,16 @@
+var db = require('./db');
+db.open('taglite.dbasdasd');
+console.log(db);
+var orig = 'Paul';
+var modified = 'PaulieBoy!!!';
+console.log('Renaming tag:');
+console.log('renameTag retval: '+db.renameTag(orig, modified));
+//console.log('Removing path: /path/to/');
+//console.log('removePath retval: '+db.removePath('/path/to/'));
+
+//console.log('updateFile: '+db.updateFile('/path/to/file/here.png', ['mike', 'scott'], 'My caption.'));
+var paths = [];
+db.search('Paul', paths)
+console.log('Searching: '+paths);
+
+close(db);
